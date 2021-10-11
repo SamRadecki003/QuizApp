9357b70f97edc9624ce975230ae8cac47df3abf5
Samantha Radecki <samantha.radecki@gmail.com>
2021-10-11 15:34:03 -0500

    able to see the questions on page

--- src/App.tsx
+++ src/App.tsx
@@ -27,10 +27,10 @@ function App() {
     );
 
     setQuestions(newQuestions);
-    setScore(0)
-    setUserAnswers([])
-    setNumber(0)
-    setLoading(false)
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
   };
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
@@ -40,22 +40,31 @@ function App() {
   return (
     <div className="App">
       <h1>React Quiz</h1>
-      <button className="start" onClick={startTrivia}>
-        START
-      </button>
-      <p className="score">Score: </p>
-      <p>Loading Questions...</p>
-      {/* <QuestionCard 
-        questionNum ={number+1}
-        totalQuestions = {TOTAL_QUESTIONS}
-        question={questions[number].question}
-        answers={questions[number].answers}
-        userAnswer = {userAnswers ? userAnswers[number]: undefined}
-        callback={checkAnswer}
-      /> */}
-      <button className="next" onClick={nextQuestion}>
-        Next Question
-      </button>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className="start" onClick={startTrivia}>
+          START
+        </button>
+      ) : null}
+      {!gameOver ? <p className="score">Score: </p> : null}
+      {loading && <p>Loading Questions...</p>}
+      {!loading && !gameOver && (
+        <QuestionCard
+          questionNum={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+      )}
+      {!gameOver &&
+      !loading &&
+      userAnswers.length === number + 1 &&
+      number !== TOTAL_QUESTIONS - 1 ? (
+        <button className="next" onClick={nextQuestion}>
+          Next Question
+        </button>
+      ) : null}
     </div>
   );
 }
